0d328085f48abda94e7e716655c0e46dcef24e23
Rune Finstad Halvorsen <runefh@gmail.com>
2020-02-12 11:32:48 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { createApiCall } from './builder';
 export { ApiError } from './exceptions';
 export { Config } from './types';
 export { makeDef, argType } from './builder4';
--- src/types.ts
+++ src/types.ts
@@ -22,19 +22,6 @@ export type FormBodyBuilder<A> = (args: A) => FormBody;
 export type JsonBody = Record<string, unknown>;
 export type JsonBodyBuilder<A> = (args: A) => JsonBody;
 
-export interface CallRecord<A, R> {
-  argRuntype?: rt.Runtype;
-  headersBuilder?: HeadersBuilder<A>;
-  httpMethod?: Method;
-  outputRuntype: rt.Runtype<R>;
-  pathBuilder?: PathBuilder<A>;
-  queryBuilder?: QueryBuilder<A>;
-  mapper?: (response: R, args: A) => any;
-  bodyBuilder?:
-    | { kind: 'form'; builder: FormBodyBuilder<A> }
-    | { kind: 'json'; builder: JsonBodyBuilder<A> };
-}
-
 interface OnBeforeInfo {
   startTimeMs: number;
 }
